2d2e9f4e92d612b557c79cb820028f1feacc3146
Vinicius Stock <vinicius.stock@shopify.com>
2022-06-13 09:28:46 -0400

    Disable format on save

--- src/configuration.ts
+++ src/configuration.ts
@@ -24,7 +24,7 @@ export const DEFAULT_CONFIGS = [
     scope: { languageId: "ruby" },
     section: "editor",
     name: "formatOnSave",
-    value: true,
+    value: false,
   },
   { section: "byesig", name: "fold", value: false },
   { section: "byesig", name: "enabled", value: true },
