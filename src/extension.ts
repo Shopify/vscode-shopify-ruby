fbf1767f1cfd0f7fc65d7ed80018a0d937a7e974
Vinicius Stock <vinicius.stock@shopify.com>
2024-07-08 17:11:35 -0400

    Remove setting recommendation engine

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,41 @@
 import * as vscode from "vscode";
 
-import { Configuration } from "./configuration";
-
 export async function activate(context: vscode.ExtensionContext) {
-  const configuration = new Configuration(vscode.workspace, context);
-  await configuration.applyDefaults();
-  await configuration.offerTheme();
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      "rubyExtensionsPack.forceApplyDefaults",
-      () => configuration.applyDefaults(true),
-    ),
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand("rubyExtensionsPack.clearState", () =>
-      configuration.clearState(),
-    ),
-  );
+  await offerTheme(context);
 }
 
 export function deactivate() {}
+
+async function offerTheme(context: vscode.ExtensionContext) {
+  const cacheKey = "shopify.ruby-extensions-pack.offeredTheme";
+
+  // Avoid offering to use the new theme more than once
+  if (context.globalState.get(cacheKey)) {
+    return;
+  }
+
+  const config = vscode.workspace.getConfiguration("workbench");
+  const theme = config.get("colorTheme");
+
+  // If the user is already using the new theme, don't offer it
+  if (theme !== "Spinel") {
+    await context.globalState.update(cacheKey, true);
+    return;
+  }
+
+  const response = await vscode.window.showInformationMessage(
+    "The Spinel theme is tailored for better visuals when working with Ruby. Would you like to try it?",
+    "Yes",
+    "No",
+  );
+
+  if (response === "Yes") {
+    await config.update(
+      "colorTheme",
+      "Spinel",
+      vscode.ConfigurationTarget.Global,
+    );
+  }
+
+  await context.globalState.update(cacheKey, true);
+}
